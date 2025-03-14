1d1793a83cdd27dcdc0d07f66efedb29e309bc69
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-14 20:11:40 +0500

    worked on tokenization and stop words NLP

--- sentiment analysis/main.py
+++ sentiment analysis/main.py
@@ -1,18 +1,27 @@
-# cleaning text steps
-# 1) create a tex file and take from it
-# 2) convert the letter into lowercase 
-# 3) remove the punctuation
-
 import string
 text = open('text.txt', encoding='utf-8').read()
 lower_case = text.lower()
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+tokenized_word = cleaned_text.split()
+print(tokenized_word)
 
-# str1 : Specifies the list of characters that need to be replaced.
-# str2 : Specifies the list of characters with which the characters need to be replaced.
-# str3 : Specifies the list of characters that needs to be deleted.
-# str1 = 'abc'
-# str2 = 'gef'
-# returns : Returnsthe translation table which specifies the conversatios that can be used in translate() function.
-print(string.punctuation)
-cleaned_text = lower_case.translate(str.marketrans('', '', string.punctuation))
-print(cleaned_text)
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+
+final_words = []
+for word in tokenized_word:
+    if word not in stop_words:
+        final_words.append(word)
+
+
+print(final_words)
