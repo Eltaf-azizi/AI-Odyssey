c87fc1f81280f53ad9d541c2ee7aa5891437a9c4
altaf azizi <iltfhuss@gmail.com>
2025-03-14 18:50:07 +0500

    cleaning the text for NLP

--- sentiment analysis/main.py
+++ sentiment analysis/main.py
@@ -3,5 +3,16 @@
 # 2) convert the letter into lowercase 
 # 3) remove the punctuation
 
-text = open('read.txt', encoding='utf-8').read()
-print(text)
+import string
+text = open('text.txt', encoding='utf-8').read()
+lower_case = text.lower()
+
+# str1 : Specifies the list of characters that need to be replaced.
+# str2 : Specifies the list of characters with which the characters need to be replaced.
+# str3 : Specifies the list of characters that needs to be deleted.
+# str1 = 'abc'
+# str2 = 'gef'
+# returns : Returnsthe translation table which specifies the conversatios that can be used in translate() function.
+print(string.punctuation)
+cleaned_text = lower_case.translate(str.marketrans('', '', string.punctuation))
+print(cleaned_text)
