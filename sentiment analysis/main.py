941e3119aa4b480c8ad1d68df9e660d40e9f6514
altaf azizi <iltfhuss@gmail.com>
2025-03-14 17:59:37 +0500

    start working on a sentiment analysis

---
+++ sentiment analysis/main.py
@@ -0,0 +1,7 @@
+# cleaning text steps
+# 1) create a tex file and take from it
+# 2) convert the letter into lowercase 
+# 3) remove the punctuation
+
+text = open('read.txt', encoding='utf-8').read()
+print(text)
