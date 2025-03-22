d9de378bbe7daf34462aa47ed4df04aa7d25f15c
Eltaf-azizi <iltfhuss@gmail.com>
2025-03-22 16:40:44 +0500

    working on the twitter emotion analysis

--- sentiment analysis/twitter_analysis.py
+++ sentiment analysis/twitter_analysis.py
@@ -1,10 +1,79 @@
 import GetOldTweets3 as got
+import string
+from typing import Counter
+import matplotlib.pyplot as plt
+
+text = open('text.txt', encoding='utf-8').read()
+lower_case = text.lower()
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+tokenized_word = cleaned_text.split()
+
 
 
 def get_tweets():
-    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees') \ 
-        .setSince("2015-05-01") \ 
-        .setUntil("2015-09-30") \
-        .setMaxTweets(1)
-    tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-    print(tweet.text)
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('corona virus') \ 
+        .setSince("2019-05-01") \ 
+        .setUntil("2020-09-30") \
+        .setMaxTweets(10)
+    
+    #List of object gets stored in 'tweets' variable
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+    # Iterating through tweets list. Storing them temp in tweet variable.
+    # Get text and store it as a list inside text_tweets
+    text_tweets = [[tweet.text] for tweet in tweets]
+    return text_tweets
+
+
+get_tweets()
+
+
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+
+final_words = []
+for word in tokenized_word:
+    if word not in stop_words:
+        final_words.append(word)
+
+
+# NLP emotion algorithm
+# 1) check if the word in the final word list is also present in emotion.txt
+# - open the emotion file
+# - loop through each line and clear it
+# - extract the word and emotion using split
+
+# 2) If word is present -> Add the emotion to emotion_list
+# 3) Finally count each emotion in the emotion list
+
+
+emotion_list = []
+with open('emotion.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_words:
+            emotion_list.append(emotion)
+
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
