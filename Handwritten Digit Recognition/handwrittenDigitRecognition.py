9d5183004ffb344031cc6ab52c1911ed96ed8a7c
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-10 23:41:07 +0500

    adding the images

--- Handwritten Digit Recognition/handwrittenDigitRecognition.py
+++ Handwritten Digit Recognition/handwrittenDigitRecognition.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 
 
@@ -29,7 +29,3 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.load_modal('handwritten.model')
 
-loss, accuracy = model.evaluate(x_test, y_test)
-
-print(loss)
-print(accuracy)
