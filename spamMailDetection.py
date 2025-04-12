f6531a99a3a45eb433db78627dee3006f7ceb019
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-12 19:41:27 +0500

    working on the email_text

--- spamMailDetection.py
+++ spamMailDetection.py
@@ -27,3 +27,21 @@ for i in range(len(df)):
     text = [stemmer.stem(word) for word in text if word not in stopwords_set]
     text = ' '.join(text)
     corpus.append(text)
+
+
+vectorizer = CountVectorizer()
+
+x = vectorizer.fit_transform(corpus).toarray()
+y = df.label_num
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+clf = RandomForestClassifier(n_jobs=-1)
+
+clf.fit(x_train, y_train)
+clf.score(x_test, y_test)
+
+
+email_text = email_to_classify.lower().translate(str.maketrans('', '', string.punctuation)).split()
+email_text = [stemmer.stem(word) for word in text if word not in stopwords_set]
+email_text = ' '.join(email_text)
