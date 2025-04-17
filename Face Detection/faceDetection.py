25a0844c5e30f83f31428159317e4a97026af26d
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-17 21:56:09 +0500

    created the face detector

--- Face Detection/faceDetection.py
+++ Face Detection/faceDetection.py
@@ -12,4 +12,16 @@ while True:
 
 
     for(x1, y1, w1, h1) in f:
-        cv2.rectangle(d_image, (x1, y1), (x1+w1, y1+h1), (255, 0, 0), 5)
+        cv2.rectangle(d_image, (x1, y1), (x1+w1, y1+h1), (0, 255, 0), 5)
+
+
+    
+    cv2.imshow('img', d_image)
+    h = cv2.waitKey(40) & 0xff
+
+    if h == 40:
+        break
+
+
+b.release()
+cv2.destryAllWindows()
